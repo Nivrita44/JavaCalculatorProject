c4b595b69cb8c7ec9bda55ed7e6a1d2713593af1
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2023-06-05 23:16:27 +0600

    calculation

--- JavaCalculator1/src/main/java/com/example/javacalculator1/HelloApplication.java
+++ JavaCalculator1/src/main/java/com/example/javacalculator1/HelloApplication.java
@@ -1,22 +1,22 @@
 package com.example.javacalculator1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 388, 574);
         stage.setTitle("Calculator");
-        stage.setScene(new Scene(root));
+        stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
--- JavaCalculator1/src/main/java/com/example/javacalculator1/HelloController.java
+++ JavaCalculator1/src/main/java/com/example/javacalculator1/HelloController.java
@@ -1,14 +1,71 @@
 package com.example.javacalculator1;
 
+import java.net.URL;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.util.ResourceBundle;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+//import static com.sun.javafx.css.FontFaceImpl.FontFaceSrcType.URL;
 
+public class HelloController implements Initializable {
+    //@FXML
+    //private Label welcomeText;
+
+    // @FXML
+    //protected void onHelloButtonClick() {
+    // welcomeText.setText("Welcome to JavaFX Application!");}
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField txt_result;
+    String op = "";
+    long num1;
+    long num2;
+
+    public void Number(ActionEvent ae) {
+        String no = ((Button) ae.getSource()).getText();
+        txt_result.setText(txt_result.getText() + no);
     }
-}
+
+    public void Operation(ActionEvent ae) {
+        String operation = ((Button) ae.getSource()).getText();
+        if (!operation.equals("=")) {
+            if (!op.equals("")) {
+                return;
+            }
+            op = operation;
+            num1 = Long.parseLong(txt_result.getText());
+            txt_result.setText("");
+        } else {
+            if (!op.equals("")) {
+                return;
+            }
+            //op = operation;
+            num2 = Long.parseLong(txt_result.getText());
+            calculate(num1, num2, op);
+            op = "";
+        }
+    }
+
+    public void calculate(long n1, long n2, String op) {
+        switch (op) {
+            case "+": txt_result.setText(n1 + n2 + "");break;
+            case "-": txt_result.setText(n1 - n2 + "");break;
+            case "x": txt_result.setText(n1 * n2 + "");break;
+            case "/":
+                if (n2 == 0) {
+                    txt_result.setText("0");
+                    break;
+                }
+                txt_result.setText(n1 / n2 + "");
+                break;
+        }
+
+    }
+
+        @Override
+        public void initialize (URL url, ResourceBundle resourcebundle){
+
+        }
+    }
