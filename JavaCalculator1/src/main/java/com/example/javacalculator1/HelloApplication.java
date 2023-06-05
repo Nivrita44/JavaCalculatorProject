74f4bc46b0d77ba15d806b6a40ee59b26b01c770
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2023-06-05 18:37:42 +0600

    frame

--- JavaCalculator1/src/main/java/com/example/javacalculator1/HelloApplication.java
+++ JavaCalculator1/src/main/java/com/example/javacalculator1/HelloApplication.java
@@ -1,23 +1,22 @@
 package com.example.javacalculator1;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        stage.setTitle("Calculator");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
